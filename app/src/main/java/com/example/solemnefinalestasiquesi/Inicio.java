c674b365c76c0110856bf0c7c3136d5227c98933
Ignacio Montano <ignamonv@gmail.com>
2019-12-19 21:51:14 -0300

    Initial commit

---
+++ app/src/main/java/com/example/solemnefinalestasiquesi/Inicio.java
@@ -0,0 +1,37 @@
+package com.example.solemnefinalestasiquesi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Inicio extends AppCompatActivity {
+
+    Button btnLogOut;
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+
+
+        btnLogOut=findViewById(R.id.logOut);
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intToRegistrar = new Intent(Inicio.this, MainActivity.class);
+                startActivity(intToRegistrar);
+            }
+        });
+    }
+
+
+}
