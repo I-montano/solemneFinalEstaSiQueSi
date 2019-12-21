8a8f7010a47dab0a48618202547e56af9ddae05a
JavierPoduje <javier.poduje@gmail.com>
2019-12-21 10:12:35 -0300

    feat: add ChatApp

--- app/src/main/java/com/example/solemnefinalestasiquesi/DataBase/ChatApp.java
+++ app/src/main/java/com/example/solemnefinalestasiquesi/DataBase/ChatApp.java
@@ -3,7 +3,32 @@ package com.example.solemnefinalestasiquesi.DataBase;
 import android.app.Application;
 import android.content.Context;
 
+import androidx.room.Room;
+
 public class ChatApp extends Application {
     private static Context context;
     private static AppDatabase database;
+
+    public void onCreate() {
+        super.onCreate();
+
+        ChatApp.context = getApplicationContext();
+        ChatApp.database = initDatabase();
+    }
+
+    public static Context getAppContext() {
+        return ChatApp.context;
+    }
+
+    public static AppDatabase getDatabase() {
+        return ChatApp.database;
+    }
+
+    private AppDatabase initDatabase() {
+        AppDatabase database = Room.databaseBuilder(getAppContext(), AppDatabase.class, "shoppingcart.db")
+                .allowMainThreadQueries()   //Allows room to do operation on main thread
+                .fallbackToDestructiveMigration()
+                .build();
+        return database;
+    }
 }
